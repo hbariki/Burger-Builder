2482da18bc42c8ce4fa67915ba8b5d947b41183a
Harshini Bariki <hbariki@Harshinis-MacBook-Pro.local>
2020-09-09 14:08:43 -0700

    Added Buid Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     // Ingredient Object will be converted into Array ["salad", "bacon", "cheese", "meat"]
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         //console.log([...Array(props.ingredients[igKey])]);
         return [...Array(props.ingredients[igKey])].map(( _, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    }).reduce ((arr, el) => {
+        return arr.concat(el)
+    }, []);
     console.log(transformedIngredients);
+     if(transformedIngredients.length === 0) {
+         transformedIngredients = <p>Please start adding elements</p>;
+     }
     
     return (
         <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBulider.js
+++ src/containers/BurgerBuilder/BurgerBulider.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import BuildControl from '../../components/Burger/BuildControls/BuildControl/BuildControl';
+
+
 
 class BurgerBuilder extends Component {
     state = {
         ingredients : {
-            salad : 1,
-            bacon : 1,
-            cheese : 2,
+            salad : 0,
+            bacon : 0,
+            cheese : 0,
             meat: 2
         }
     }
@@ -15,7 +19,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Aux>
         );
     }
